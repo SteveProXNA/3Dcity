b3499fb2bd503e2ae29cabffddf73e0637927eb3
SteveProXNA <steven_boland@hotmail.com>
2019-11-01 17:55:35 +0000

    Upload all files

--- 3Dcity.IOS/3Dcity.IOS/Program.cs
+++ 3Dcity.IOS/3Dcity.IOS/Program.cs
@@ -4,28 +4,28 @@ using UIKit;
 namespace WindowsGame
 {
     [Register("AppDelegate")]
-	class Program : UIApplicationDelegate
-	{
-		private static WindowsGame.Common.AnGame game;
+    class Program : UIApplicationDelegate
+    {
+        private static WindowsGame.Common.AnGame game;
 
-		internal static void RunGame()
-		{
-			game = new WindowsGame.Common.AnGame();
-			game.Run();
-		}
+        internal static void RunGame()
+        {
+            game = new WindowsGame.Common.AnGame();
+            game.Run();
+        }
 
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		static void Main(string[] args)
-		{
-			UIApplication.Main(args, null, "AppDelegate");
-		}
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            UIApplication.Main(args, null, "AppDelegate");
+        }
 
-		public override void FinishedLaunching(UIApplication app)
-		{
-			RunGame();
-		}
+        public override void FinishedLaunching(UIApplication app)
+        {
+            RunGame();
+        }
 
-	}
+    }
 }
